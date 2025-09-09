29c437208d37efa219209d164337589fb46a7fc0
Michael Cook <entozoon@gmail.com>
2025-09-09 20:30:35 +0100

    feat: led pixel address map

--- src/led-test.cpp
+++ src/led-test.cpp
@@ -1,42 +1,25 @@
-// #include <Adafruit_NeoPixel.h>
+#include <Adafruit_NeoPixel.h>
 
-// #define PIN 7
-// #define NUMPIXELS 16 // 4x4 matrix
+#define PIN 10
+#define NUMPIXELS 16 // 4x4 matrix
 
-// Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-// // Helper function to generate rainbow colors
-// uint32_t Wheel(byte WheelPos)
-// {
-//   WheelPos = 255 - WheelPos;
-//   if (WheelPos < 85)
-//   {
-//     return pixels.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-//   }
-//   if (WheelPos < 170)
-//   {
-//     WheelPos -= 85;
-//     return pixels.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-//   }
-//   WheelPos -= 170;
-//   return pixels.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-// }
+const int ledOrder[16] = {3, 2, 1, 0, 7, 6, 5, 4, 11, 10, 9, 8, 15, 14, 13, 12};
 
-// void setup()
-// {
-//   pixels.begin();
-//   pixels.setBrightness(50); // Set brightness to a moderate level
-// }
+void setup()
+{
+  pixels.begin();
+  pixels.setBrightness(50); // Set brightness to a moderate level
+}
 
-// void loop()
-// {
-//   for (int j = 0; j < 256; j++)
-//   { // Cycle through all 256 colors in the wheel
-//     for (int i = 0; i < NUMPIXELS; i++)
-//     {
-//       pixels.setPixelColor(i, Wheel((i + j) & 255));
-//     }
-//     pixels.show();
-//     delay(20);
-//   }
-// }
+void loop()
+{
+  for (int i = 0; i < NUMPIXELS; i++)
+  {
+    pixels.clear();                                                 // Turn off all pixels
+    pixels.setPixelColor(ledOrder[i], pixels.Color(255, 255, 255)); // Turn on the current pixel (white)
+    pixels.show();                                                  // Update the strip
+    delay(1000);                                                    // Wait for one second
+  }
+}
